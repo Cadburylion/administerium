8052678162149ad83419782ecdb8df9a7e31d79b
Matthew Parker <cadburylion@outlook.com>
2018-07-21 18:01:06 -0700

    Fixed typo and changed script to run server with nodemon

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,13 +1,13 @@
-export const AUTH_USER = 'auth_user';
-export const AUTH_ERROR = 'auth_error';
+export const AUTH_USER = 'AUTH_USER';
+export const AUTH_ERROR = 'AUTH_ERROR';
 
-export const CLEAR_BANNED_USERS = 'clear_banned_users';
-export const ADD_BANNED_USER = 'add_banned_user';
+export const CLEAR_BANNED_USERS = 'CLEAR_BANNED_USERS';
+export const ADD_BANNED_USER = 'ADD_BANNED_USER';
 export const GET_BANNED_USERS_START = 'GET_BANNED_USERS_START';
-export const GET_BANNED_USERS_SUCCESS = 'GET_BANNED_USERS_SUCESS';
+export const GET_BANNED_USERS_SUCCESS = 'GET_BANNED_USERS_SUCCESS';
 export const GET_BANNED_USERS_FAILURE = 'GET_BANNED_USERS_FAILURE';
-export const GET_BANNED_USER_BY_NAME = 'get_banned_user_by_name';
-export const GET_BANNED_USER_BY_NAME_ERROR = 'get_banned_user_by_name_error';
+export const GET_BANNED_USER_BY_NAME = 'GET_BANNED_USER_BY_NAME';
+export const GET_BANNED_USER_BY_NAME_ERROR = 'GET_BANNED_USER_BY_NAME_ERROR';
 
 export const REMOVE_FROM_BANNED_LIST_START = 'REMOVE_FROM_BANNED_LIST_START';
 export const REMOVE_FROM_BANNED_LIST_SUCCESS =
