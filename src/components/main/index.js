f5a2d6a44713cbd593d865ef751f01aefebca34e
Matthew Parker <cadburylion@outlook.com>
2019-01-29 17:40:32 -0800

    Added styled-components, modified main component

--- src/components/main/index.js
+++ src/components/main/index.js
@@ -1,35 +1,34 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
-import { Route } from 'react-router-dom';
+import React from "react";
+import { connect } from "react-redux";
+import { Route } from "react-router-dom";
+import styled from "styled-components";
 
-import Header from '../header';
-import Search from '../search';
-import Signout from '../auth/signout';
-import BannedList from '../banned-list';
-import BannedSubmitForm from '../banned-submit-form';
-import requireAuth from '../require-auth';
+import Header from "../header";
+import Search from "../search";
+import Signout from "../auth/signout";
+import BannedList from "../banned-list";
+import BannedSubmitForm from "../banned-submit-form";
+import requireAuth from "../require-auth";
 
-import { getAuth } from '../../selectors';
+import { getAuth } from "../../selectors";
 
-class Main extends Component {
-  render() {
-    return (
-      <main
-        style={{
-          maxWidth: '680px',
-          margin: '0 auto',
-          minHeight: '100vh',
-          overflowX: 'hidden'
-        }}
-      >
-        <Header />
-        <Route exact path="/app" component={BannedList} />
-        <Route exact path="/app/youarebanned" component={BannedSubmitForm} />
-        <Route exact path="/app/signout" component={Signout} />
-        <Route exact path="/app/search" component={Search} />
-      </main>
-    );
-  }
+const MainDiv = styled.div`
+  max-width: 680px;
+  margin: 0 auto;
+  min-height: 100vh;
+  overflow-x: hidden;
+`;
+
+function Main() {
+  return (
+    <MainDiv>
+      <Header />
+      <Route exact path="/app" component={BannedList} />
+      <Route exact path="/app/youarebanned" component={BannedSubmitForm} />
+      <Route exact path="/app/signout" component={Signout} />
+      <Route exact path="/app/search" component={Search} />
+    </MainDiv>
+  );
 }
 
 const mapStateToProps = state => ({
